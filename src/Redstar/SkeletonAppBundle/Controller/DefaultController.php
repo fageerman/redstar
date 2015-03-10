a302f4a9f95a62ed4bedd803b147a581f8ecd261
fageerman <f.a.geerman@gmail.com>
2015-03-10 13:16:41 +0100

    SkeletonBundle added.

--- src/Redstar/SkeletonAppBundle/Controller/DefaultController.php
+++ src/Redstar/SkeletonAppBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace Redstar\SkeletonAppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('RedstarSkeletonAppBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function editAction(){
+        return new Response('edit page');
+    }
+    
+    public function homeAction(){
+        return $this->render('RedstarSkeletonAppBundle:Default:home.html.twig');
     }
 }
