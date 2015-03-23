3e96190525587e9d7047107c0c8db012f93bf5ea
fageerman <f.a.geerman@gmail.com>
2015-03-23 16:24:11 +0100

    reset router name in menubuilder

--- src/Redstar/SkeletonAppBundle/Menu/MenuBuilder.php
+++ src/Redstar/SkeletonAppBundle/Menu/MenuBuilder.php
@@ -19,22 +19,6 @@ class MenuBuilder extends ContainerAware
         $menu->addChild('Dashboard', array('route' => 'redstar_skeleton_app_homepage'));
         $menu->addChild('Usermanagement', array('route' => 'redstar_user_homepage'));
 
-
-//        // access services from the container!
-//        $em = $this->container->get('doctrine')->getManager();
-//        // findMostRecent and Blog are just imaginary examples
-//        $blog = $em->getRepository('AppBundle:Blog')->findMostRecent();
-//
-//        $menu->addChild('Latest Blog Post', array(
-//            'route' => 'blog_show',
-//            'routeParameters' => array('id' => $blog->getId())
-//        ));
-//
-//        // you can also add sub level's to your menu's as follows
-//        $menu['About Me']->addChild('Edit profile', array('route' => 'edit_profile'));
-
-        // ... add more children
-
         return $menu;
     }
 }
