b58f7e4d2cb5d42ef99961082a1c52648f493a26
fageerman <f.a.geerman@gmail.com>
2015-06-12 13:00:40 -0400

    Code fix for previous commit

--- src/Redstar/UserBundle/Controller/UserController.php
+++ src/Redstar/UserBundle/Controller/UserController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
+    /*
+     * 
+     * List all users from the database 
+     */
     public function indexAction()
     {
+        
         return $this->render('RedstarUserBundle:Default:index.html.twig');
     }
 }
