96660130a9fdad7df575fef40d92c8f6f92d918b
fageerman <f.a.geerman@gmail.com>
2015-06-12 12:56:07 -0400

    rename DefaultController to UserController in UserBundle

--- src/Redstar/UserBundle/Controller/UserController.php
+++ src/Redstar/UserBundle/Controller/UserController.php
@@ -4,7 +4,7 @@ namespace Redstar\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class UserController extends Controller
 {
     public function indexAction()
     {
